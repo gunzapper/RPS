830b27e71a0ad9aa9f148fafa3e7868a9ca903d7
Pietro Brunetti <gunzapper@gmail.com>
2015-12-08 16:17:20 +0100

    Improving outputs + Future plan
    I decorate a bit the results
    I also write a future plan to continue the development
    I have to review the code

--- README.md
+++ README.md
@@ -2,8 +2,23 @@
 
 This is a cli implementation of Rock Paper Scissor (in Italian 'Morra Cinese').
 
-Requirements:
+## Requirements:
 
   - docopt
       
         $ pip3 install docopt
+
+
+## Future plan
+
+Next simple improvements:
+Use also "paper" or "Paper" for 'P' and so on..
+
+Future version:
+
+    - version 1: iteractive version - using coroutines
+    - version 2: improving game strategy - not a simple random choice
+    - version 3: learn from player
+
+experimental version:
+Introduce "Spok" and "Lizard" 
--- RPS/RPS.py
+++ RPS/RPS.py
@@ -4,9 +4,7 @@
 """Rock - Paper - Scissor game.
 
 Usage:
-  RPS.py P
-  RPS.py S
-  RPS.py R
+  RPS.py <hand>
   RPS.py -h | --help
   RPS.py -t | --test
   RPS.py --version
@@ -17,6 +15,8 @@ Options:
   --version     show the version.
 """
 
+import random
+
 from docopt import docopt
 
 GAME_TABLE = {
@@ -25,9 +25,42 @@ GAME_TABLE = {
     'S': 2
 }
 
+Nice_comments = {
+    'R': "Rock breaks scissor",
+    'S': "Scissor cuts paper",
+    'P': "Paper lines rock"
+}
+
+results = [
+    "No one wins",
+    "You won",
+    "I beated you"
+]
+
 
 def who_wins(hand1, hand2):
     """
+    Determine who wins at rock paper scissor game using Modular matematic
+
+    In this function:
+        * 'R' accounts for 'rock'
+        * 'P' accounts for 'paper'
+        * 'S' accounts for 'scissor'
+
+    Args:
+        hand1 (str): The hand of the first player
+        hand2 (str): The hand of the second player
+
+    Returns:
+        int.  The return code::
+
+          0 -- Pair
+          1 -- first player won.
+          2 -- second player won.
+
+    Raises:
+        ValueError
+
     >>> who_wins('R', 'S')
     1
     >>> who_wins('P', 'R')
@@ -57,13 +90,28 @@ def who_wins(hand1, hand2):
         raise ValueError("only 'R', 'P' and 'S' are allowed input")
 
 
-def main():
-    pass
+def main(args):
+
+    if args['--test']:
+        print('testing...')
+        import doctest
+        doctest.testmod()
+
+    if args['<hand>']:
+        random.seed()
+        bots_hand = random.choice('SPR')
+        print("You played {0}\nI played {1}".format(
+            args['<hand>'], bots_hand)
+        )
+        point = who_wins(args['<hand>'], bots_hand)
+        print(results[point])
+        if point == 1:
+            print(Nice_comments[args['<hand>']])
+        elif point == 2:
+            print(Nice_comments[bots_hand])
+
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
 
     arguments = docopt(__doc__, version='Rock Paper Scissor 0.0.1')
-    print(arguments)
-    main()
+    main(arguments)
