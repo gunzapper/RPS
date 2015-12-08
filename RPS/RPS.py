0936d53e469bc72dc396e3ce10cee8b92891266b
Pietro Brunetti <gunzapper@gmail.com>
2015-12-08 11:17:17 +0100

    I use the module math to determine who wins
    The engine is simple - but there is a probably vicius:
    Can I extend the game with Spock and Lizard?

--- RPS/RPS.py
+++ RPS/RPS.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 
 
+GAME_TABLE = {
+    'R': 0,
+    'P': 1,
+    'S': 2
+}
+
+
 def who_wins(hand1, hand2):
     """
     >>> who_wins('R', 'S')
@@ -9,14 +16,17 @@ def who_wins(hand1, hand2):
     >>> who_wins('R', 'P')
     2
     >>> who_wins('R', 'R')
-    3
+    0
     >>> who_wins('R', '?')
     Traceback (most recent call last):
         ...
     ValueError: only 'R', 'P' and 'S' are allowed input
     """
+    try:
+        return (GAME_TABLE[hand1] - GAME_TABLE[hand2]) % 3
+    except KeyError:
+        raise ValueError("only 'R', 'P' and 'S' are allowed input")
 
-    pass
 
 def main():
     pass
